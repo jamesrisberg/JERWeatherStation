c21fbca76f215dda225f91b8653f2bbdc36bcf5e
James Risberg <jimmyrisberg56@gmail.com>
2016-03-16 18:45:39 -0400

    Update endpoints

--- public/weather.js
+++ public/weather.js
@@ -4,11 +4,11 @@ var weatherStation = angular.module('weatherStation', []);
 
 angular.module('weatherStation').controller('weatherController', ['$scope', '$http',
   function ($scope, $http) {
-    var photoCellEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/photoResistorValue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
+    var photoCellEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/light?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
     
-    var temperatureEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/temperatureValue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
+    var temperatureEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/temperature?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
     
-    var humidityEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/humidityValue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
+    var humidityEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/humidity?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
 
     $http({
       method: 'GET',
