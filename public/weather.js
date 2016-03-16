bd316ce474168ea38440aba814ffa5a1868d1e93
James Risberg <jimmyrisberg56@gmail.com>
2016-03-16 05:46:53 -0400

    Log response

--- public/weather.js
+++ public/weather.js
@@ -10,10 +10,11 @@ var weatherStation = angular.module('WeatherStation', []);
 
 var photoCellEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/analogvalue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
 
-function mainController($scope, $http) {
+function weatherController($scope, $http) {
   $http.get(photoCellEndpoint)
       .success(function(data){
         $scope.response = data;
+        console.log('Response: \n' + data);
       })
       .error(function(data) {
         console.log('Error: ' + data);
