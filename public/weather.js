daa8fe201c6b7a95029e2447650353b428fc139d
James Risberg <jimmyrisberg56@gmail.com>
2016-03-16 05:55:53 -0400

    Added request library

--- public/weather.js
+++ public/weather.js
@@ -1,22 +1,12 @@
-//var request = require("request");
-//
-
-//
-//request(photoCellEndpoint, function(error, response, body) {
-//  console.log(body);
-//});
+var request = require("request");
 
 var weatherStation = angular.module('weatherStation', []);
 
 var photoCellEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/analogvalue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
 
 function weatherController($scope, $http) {
-  $http.get(photoCellEndpoint)
-      .success(function(data){
-        $scope.response = data;
-        console.log('Response: \n' + data);
-      })
-      .error(function(data) {
-        console.log('Error: ' + data);
-      });
+  request(photoCellEndpoint, function(error, response, body) {
+    $scope.response = body;
+    console.log('Response: \n' + data);
+  });
 }
