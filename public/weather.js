004cec1f468ce505ae70e00da70c63253109e816
James Risberg <jimmyrisberg56@gmail.com>
2016-03-16 05:43:49 -0400

    Even simpler setup

---
+++ public/weather.js
@@ -0,0 +1,21 @@
+//var request = require("request");
+//
+
+//
+//request(photoCellEndpoint, function(error, response, body) {
+//  console.log(body);
+//});
+
+var weatherStation = angular.module('WeatherStation', []);
+
+var photoCellEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/analogvalue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
+
+function mainController($scope, $http) {
+  $http.get(photoCellEndpoint)
+      .success(function(data){
+        $scope.response = data;
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+      });
+}
