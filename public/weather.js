71a9052a73519f0d14808dbf29d08350a960ef19
James Risberg <jimmyrisberg56@gmail.com>
2016-03-16 18:17:38 -0400

    Add DHT sensor

--- public/weather.js
+++ public/weather.js
@@ -4,14 +4,38 @@ var weatherStation = angular.module('weatherStation', []);
 
 angular.module('weatherStation').controller('weatherController', ['$scope', '$http',
   function ($scope, $http) {
-    var photoCellEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/analogvalue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
+    var photoCellEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/photoResistorValue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
+    
+    var temperatureEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/temperatureValue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
+    
+    var humidityEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/humidityValue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
 
     $http({
       method: 'GET',
       url: photoCellEndpoint
     }).then(function successCallback(response) {
-      $scope.response = response;
-      console.log('Response: \n' + response);
+      $scope.photoCellValue = response.data.result;
+      console.log(response);
+    }, function errorCallback(response) {
+      console.log('Error: ' + response);
+    });
+    
+    $http({
+      method: 'GET',
+      url: temperatureEndpoint
+    }).then(function successCallback(response) {
+      $scope.temperatureValue = response.data.result;
+      console.log(response);
+    }, function errorCallback(response) {
+      console.log('Error: ' + response);
+    });
+    
+    $http({
+      method: 'GET',
+      url: humidityEndpoint
+    }).then(function successCallback(response) {
+      $scope.humidityValue = response.data.result;
+      console.log(response);
     }, function errorCallback(response) {
       console.log('Error: ' + response);
     });
