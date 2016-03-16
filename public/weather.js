4dd13ca06911f5f82eef7e20e6e976e23dbfef22
James Risberg <jimmyrisberg56@gmail.com>
2016-03-16 05:49:58 -0400

    Update

--- public/weather.js
+++ public/weather.js
@@ -6,7 +6,7 @@
 //  console.log(body);
 //});
 
-var weatherStation = angular.module('WeatherStation', []);
+var weatherStation = angular.module('weatherStation', []);
 
 var photoCellEndpoint = 'https://api.particle.io/v1/devices/2d0033000d47343432313031/analogvalue?access_token=81ff21f4c20d40c5727b1d366f44deb73eaee676';
 
